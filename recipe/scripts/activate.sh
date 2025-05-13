510082ed0a4f46966e36800e7cdbd7cf2529d3fe
memento <claude.rc@gmail.com>
2025-05-13 12:05:18 -0500

    (fix) odd 'cache' issue osx? local nss ppc

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -5,13 +5,15 @@ if [[ -n "$PGADMIN4_PY_HOME" ]]; then
   export _CONDA_SET_PGADMIN4_PY_HOME="$PGADMIN4_PY_HOME"
 fi
 export PGADMIN4_PY_EXEC="$CONDA_PREFIX"/bin/python
-export PGADMIN4_PY_HOME=`$CONDA_PREFIX/bin/python -c "import pgadmin4, os; print(os.path.dirname(pgadmin4.__file__))" | tail -n 1`/pgAdmin4.py
+export PGADMIN4_PY_HOME=`$CONDA_PREFIX/bin/python -c "import pgadmin4, os; print(os.path.dirname(pgadmin4.__file__))" | tail -n 1`
 
 # Special app link for MacOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  if [[ -e "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/web ]]; then
-    mv "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/web "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/_conda_set_web
+  WEB_PATH="$CONDA_PREFIX/usr/pgadmin4.app/Contents/Resources/web"
+  BACKUP_PATH="$CONDA_PREFIX/usr/pgadmin4.app/Contents/Resources/_conda_set_web"
+  if [[ -e "$WEB_PATH" || -L "$WEB_PATH" ]]; then
+    mv "$WEB_PATH" "$BACKUP_PATH"
   fi
-  ln -s "$PGADMIN4_PY_HOME" "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/web
-  ls -lrt "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/web
+  ln -s "$PGADMIN4_PY_HOME" "$WEB_PATH"
+  ls -lrt "$WEB_PATH"
 fi
--- recipe/scripts/deactivate.sh
+++ recipe/scripts/deactivate.sh
@@ -5,7 +5,11 @@ if [[ "${_CONDA_SET_PGADMIN4_PY_HOME+x}" ]] ; then
   unset _CONDA_SET_PGADMIN4_PY_HOME
 fi
 
-if [[ -L "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/_conda_set_web ]]; then
-  rm "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/web
-  mv "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/_conda_set_web "$CONDA_PREFIX"/usr/pgadmin4.app/Contents/Resources/web
+WEB_PATH="$CONDA_PREFIX/usr/pgadmin4.app/Contents/Resources/web"
+BACKUP_PATH="$CONDA_PREFIX/usr/pgadmin4.app/Contents/Resources/_conda_set_web"
+if [[ -e "$WEB_PATH" || -L "$WEB_PATH" ]]; then
+  rm -rf "$WEB_PATH"
+fi
+if [[ -e "$BACKUP_PATH" || -L "$BACKUP_PATH" ]]; then
+  mv "$BACKUP_PATH" "$WEB_PATH"
 fi
