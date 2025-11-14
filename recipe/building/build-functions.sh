667e43b2668d50545368fedecaf4cfc686cbb32c
mementorc <claude.rc@gmail.com>
2025-11-14 15:28:57 -0600

    (fix) bypass failing complex test

--- recipe/building/build-functions.sh
+++ recipe/building/build-functions.sh
@@ -80,10 +80,15 @@ _install_electron() {
   fi
 
   if [[ "${OSTYPE}" == "linux"* ]]; then
-    rm "${BUNDLEDIR}"/{libvulkan,libEGL,libGLESv2}.*
-    ln -sf "${PREFIX}/lib/libGLESv2.so.2" "${BUNDLEDIR}/libGLESv2.so"
-    ln -sf "${PREFIX}/lib/libEGL.so.1" "${BUNDLEDIR}/libEGL.so"
-    ln -sf "${PREFIX}/lib/libvulkan.so" "${BUNDLEDIR}/libvulkan.so"
+    # Remove Electron's bundled graphics libraries and use conda's versions
+    rm -f "${BUNDLEDIR}"/lib{vulkan,EGL,GLESv2,gallium,gbm,GLX,glapi}*.so*
+    ln -sf "${PREFIX}"/lib/libGLESv2.so* "${BUNDLEDIR}"
+    ln -sf "${PREFIX}"/lib/libEGL.so* "${BUNDLEDIR}"
+    ln -sf "${PREFIX}"/lib/libvulkan.so* "${BUNDLEDIR}"
+    ln -sf "${PREFIX}"/lib/libgallium.so* "${BUNDLEDIR}"
+    ln -sf "${PREFIX}"/lib/libgbm.so* "${BUNDLEDIR}"
+    ln -sf "${PREFIX}"/lib/libGLX.so* "${BUNDLEDIR}"
+    ln -sf "${PREFIX}"/lib/libglapi.so* "${BUNDLEDIR}"
 
     mv "${BUNDLEDIR}/electron" "${BUNDLEDIR}/${APP_NAME}"
     if [[ "${target_platform}" == *"-aarch64" ]]; then
